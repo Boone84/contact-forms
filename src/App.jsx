9f8fff66bdc84778bd59923a4372ccd57a66d599
boone84 <boone84@Boone-PC.localdomain>
2023-08-03 15:03:16 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>test</h1>
+      <h2>test2</h2>
+      <h1>test3</h1>
+      <h3>something</h3>
+    </>
+  )
+}
+console.log(`test`);
+
+export default App
